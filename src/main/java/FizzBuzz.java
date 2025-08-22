8aa0e75f6b9ea36c75896a39c3f81a155ec28591
DERRICK GONG (DSCOE-DSDM-ISD-OOCLL/SHA) <GONGDE@OOCL.COM>
2025-08-22 14:53:27 +0800

    Create FizzBuzz Test and FizzBuzz with 3 test cases

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+
+    public String countOff(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void count_off_normal_number() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        int number = 1;
+        assertEquals("1", fizzbuzz.countOff(number));
+    }
+
+    @Test
+    void count_off_multiple_of_3() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        int number = 3;
+        assertEquals("Fizz", fizzbuzz.countOff(number));
+    }
+
+    @Test
+    void count_off_multiple_of_5() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        int number = 5;
+        assertEquals("Buzz", fizzbuzz.countOff(number));
+    }
+}
